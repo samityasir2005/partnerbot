a00855d707f8588b43c1004b16f9c88d25b0399c
Samit <shadowrightyt@gmail.com>
2020-11-01 10:35:37 -0500

    first commit

--- main.js
+++ main.js
@@ -12,15 +12,14 @@ bot.on("ready", bot => {
     console.log('Bot is Online!')
 })
 
+const prefix = "$";
+
 bot.on("message", async message => {
-
-
-    const prefix = '$';
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
 	
-	if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '757109929281585253')) { //if it contains an invite link
+
+
+	
+	if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771052595745652776')) {
     	const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
 		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
 		const user = await Levels.fetch(message.author.id, message.guild.id);
@@ -32,13 +31,72 @@ bot.on("message", async message => {
 		.setThumbnail(message.guild.iconURL())
 	
         return message.channel.send(gaytedbed)
+	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053543838842881')) {
+		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
+		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
+		const user = await Levels.fetch(message.author.id, message.guild.id);
+		let gaytedbed = new Discord.MessageEmbed()
+		.setTitle("**StraightUp Vibe | New Partnership**")
+		.setAuthor(message.author.tag + " partnered with a new server!")
+		.setDescription("Thanks for partnering with '' ")
+		.addField("You did", user.xp + " partnership(s) in total!")
+		.setThumbnail(message.guild.iconURL())
+		return message.channel.send(gaytedbed)
+
+	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053599064457236')) {
+		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
+		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
+		const user = await Levels.fetch(message.author.id, message.guild.id);
+		let gaytedbed = new Discord.MessageEmbed()
+		.setTitle("**StraightUp Vibe | New Partnership**")
+		.setAuthor(message.author.tag + " partnered with a new server!")
+		.setDescription("Thanks for partnering with '' ")
+		.addField("You did", user.xp + " partnership(s) in total!")
+		.setThumbnail(message.guild.iconURL())
+		return message.channel.send(gaytedbed)
+
+
+	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053643469291520')) {
+		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
+		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
+		const user = await Levels.fetch(message.author.id, message.guild.id);
+		let gaytedbed = new Discord.MessageEmbed()
+		.setTitle("**StraightUp Vibe | New Partnership**")
+		.setAuthor(message.author.tag + " partnered with a new server!")
+		.setDescription("Thanks for partnering with '' ")
+		.addField("You did", user.xp + " partnership(s) in total!")
+		.setThumbnail(message.guild.iconURL())
+		return message.channel.send(gaytedbed)
+
+	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053704207400972')) {
+		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
+		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
+		const user = await Levels.fetch(message.author.id, message.guild.id);
+		let gaytedbed = new Discord.MessageEmbed()
+		.setTitle("**StraightUp Vibe | New Partnership**")
+		.setAuthor(message.author.tag + " partnered with a new server!")
+		.setDescription("Thanks for partnering with '' ")
+		.addField("You did", user.xp + " partnership(s) in total!")
+		.setThumbnail(message.guild.iconURL())
+		return message.channel.send(gaytedbed)
+
+
 	}
+	
+	if (message.content.startsWith(prefix + "phelp")) {
+		let gaytedbed2 = new Discord.MessageEmbed()
+		.setTitle("**StraightUp Vibe |Partnership Help**")
+		.setAuthor(message.author.tag)
+		.addField("$pcount", "Your total partnershipcount")
+		return message.channel.send(gaytedbed2)
+
+    }
 
     
     //Rank
-    if(command === "pcount") {
+	if (message.content.startsWith(prefix + "pcount")) {
         const user = await Levels.fetch(message.author.id, message.guild.id);
-        message.channel.send(`You currently have  **${user.xp}** partnerships!`)
+        message.channel.send(`You currently have **${user.xp}** partnerships!`)
     }
     
 })
