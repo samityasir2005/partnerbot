e2c713f1143552fd1ba0b82e278f193a89ec1f37
Samit <shadowrightyt@gmail.com>
2020-11-02 19:25:28 -0500

    first commit

--- main.js
+++ main.js
@@ -17,68 +17,106 @@ const prefix = "$";
 bot.on("message", async message => {
 	
 
-
 	
 	if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771052595745652776')) {
-    	const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
-		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
-		const user = await Levels.fetch(message.author.id, message.guild.id);
-		let gaytedbed = new Discord.MessageEmbed()
-		.setTitle("**StraightUp Vibe | New Partnership**")
-		.setAuthor(message.author.tag + " partnered with a new server!")
-		.setDescription("Thanks for partnering with '' ")
-		.addField("You did", user.xp + " partnership(s) in total!")
-		.setThumbnail(message.guild.iconURL())
-	
-        return message.channel.send(gaytedbed)
-	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053543838842881')) {
+		let invite = message.content.match(/\bhttps?:\/\/\S+/gi);
+		const d= invite.find(invite => invite.includes("discord.gg"))
+
+		let invite2 = await bot.fetchInvite(d)
+		let guild = invite2.guild
+
+		
+
 		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
 		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
 		const user = await Levels.fetch(message.author.id, message.guild.id);
+
 		let gaytedbed = new Discord.MessageEmbed()
 		.setTitle("**StraightUp Vibe | New Partnership**")
 		.setAuthor(message.author.tag + " partnered with a new server!")
-		.setDescription("Thanks for partnering with '' ")
+		.setDescription("Thanks for partnering with " + guild.name)
 		.addField("You did", user.xp + " partnership(s) in total!")
 		.setThumbnail(message.guild.iconURL())
-		return message.channel.send(gaytedbed)
+
+	
+        return bot.channels.cache.get(`772952846957412413`).send(gaytedbed)
+	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053543838842881')) {
+
+		let invite = message.content.match(/\bhttps?:\/\/\S+/gi);
+		const d= invite.find(invite => invite.includes("discord.gg"))
+
+		let invite2 = await bot.fetchInvite(d)
+		let guild = invite2.guild
+
+
+		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
+		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
+		const user = await Levels.fetch(message.author.id, message.guild.id);
+
+		let gaytedbed = new Discord.MessageEmbed()
+		.setTitle("**StraightUp Vibe | New Partnership**")
+		.setAuthor(message.author.tag + " partnered with a new server!")
+		.setDescription("Thanks for partnering with " + guild.name)
+		.addField("You did", user.xp + " partnership(s) in total!")
+		.setThumbnail(message.guild.iconURL())
+		return bot.channels.cache.get(`772952846957412413`).send(gaytedbed)
 
 	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053599064457236')) {
+		let invite = message.content.match(/\bhttps?:\/\/\S+/gi);
+		const d= invite.find(invite => invite.includes("discord.gg"))
+
+		let invite2 = await bot.fetchInvite(d)
+		let guild = invite2.guild
+
 		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
 		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
 		const user = await Levels.fetch(message.author.id, message.guild.id);
+
 		let gaytedbed = new Discord.MessageEmbed()
 		.setTitle("**StraightUp Vibe | New Partnership**")
 		.setAuthor(message.author.tag + " partnered with a new server!")
-		.setDescription("Thanks for partnering with '' ")
+		.setDescription("Thanks for partnering with " + guild.name)
 		.addField("You did", user.xp + " partnership(s) in total!")
 		.setThumbnail(message.guild.iconURL())
-		return message.channel.send(gaytedbed)
+		return bot.channels.cache.get(`772952846957412413`).send(gaytedbed)
 
 
 	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053643469291520')) {
+		let invite = message.content.match(/\bhttps?:\/\/\S+/gi);
+		const d= invite.find(invite => invite.includes("discord.gg"))
+
+		let invite2 = await bot.fetchInvite(d)
+		let guild = invite2.guild
+
 		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
 		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
 		const user = await Levels.fetch(message.author.id, message.guild.id);
+
 		let gaytedbed = new Discord.MessageEmbed()
 		.setTitle("**StraightUp Vibe | New Partnership**")
 		.setAuthor(message.author.tag + " partnered with a new server!")
-		.setDescription("Thanks for partnering with '' ")
+		.setDescription("Thanks for partnering with " + guild.name)
 		.addField("You did", user.xp + " partnership(s) in total!")
 		.setThumbnail(message.guild.iconURL())
-		return message.channel.send(gaytedbed)
+		return bot.channels.cache.get(`772952846957412413`).send(gaytedbed)
 
 	} else if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && (message.channel.id === '771053704207400972')) {
+		let invite = message.content.match(/\bhttps?:\/\/\S+/gi);
+		const d= invite.find(invite => invite.includes("discord.gg"))
+
+		let invite2 = await bot.fetchInvite(d)
+		let guild = invite2.guild
+
 		const randomXp = Math.floor(Math.random() * 0) + 1; //Random amont of XP until the number you want + 1
 		const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
 		const user = await Levels.fetch(message.author.id, message.guild.id);
 		let gaytedbed = new Discord.MessageEmbed()
 		.setTitle("**StraightUp Vibe | New Partnership**")
 		.setAuthor(message.author.tag + " partnered with a new server!")
-		.setDescription("Thanks for partnering with '' ")
+		.setDescription("Thanks for partnering with " + guild.name)
 		.addField("You did", user.xp + " partnership(s) in total!")
 		.setThumbnail(message.guild.iconURL())
-		return message.channel.send(gaytedbed)
+		return bot.channels.cache.get(`772952846957412413`).send(gaytedbed)
 
 
 	}
